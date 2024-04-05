4f1bb48bf3fd99b56542b31d9e7a5d087c3bbba9
Stefan <58773@student.eadania.dk>
2024-04-05 12:13:59 +0200

    button

--- FortressSurvivor/ComponentPattern/Button.cs
+++ FortressSurvivor/ComponentPattern/Button.cs
@@ -1,15 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace FortressSurvivor
 {
     internal class Button : Component
     {
-        public Button(GameObject gameObject) : base(gameObject)
+        private SpriteRenderer spriteRenderer;
+
+        public Button(GameObject gameObject, Rectangle bounds, Texture2D texture) : base(gameObject)
         {
+            Bounds = bounds;
+            Texture = texture;
+        }
+
+        public Rectangle Bounds { get; set; }
+        public bool IsClicked { get; private set; }
+        public Texture2D Texture { get; set; }
+        public bool IsHovered { get; private set; }
+
+        public override void Start()
+        {
+            
+            spriteRenderer = GameObject.AddComponent<SpriteRenderer>();
+            spriteRenderer.SetSprite(Texture.Name); 
+            spriteRenderer.SetLayerDepth(LAYERDEPTH.Button);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var mouseState = Mouse.GetState();
+
+            var mouseRectangle = new Rectangle(mouseState.X, mouseState.Y, 1, 1);
+
+            IsHovered = mouseRectangle.Intersects(Bounds);
+
+            if (IsHovered && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                IsClicked = true;
+            }
+            else
+            {
+                IsClicked = false;
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            
         }
     }
 }
